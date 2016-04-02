0c28761db434215abc2943078cb0fbb4d1c54928
achirkin <chirkin@arch.ethz.ch>
2016-04-03 00:08:12 +0200

    almost done uploadImages

--- apps/hs/qua-server/src/Handler/ImageUpload.hs
+++ apps/hs/qua-server/src/Handler/ImageUpload.hs
@@ -24,9 +24,10 @@ import Data.Conduit.Binary
 import Data.Default
 import Data.Text (Text)
 import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
 import qualified Data.Text.Lazy as LText
 import qualified Data.Text.Lazy.Encoding as LText
---import qualified Data.ByteString as BS
+import qualified Data.ByteString as BS
 --import qualified Data.ByteString.Lazy as BSL
 import Yesod
 import Yesod.Default.Util
@@ -39,6 +40,19 @@ import qualified Data.ByteString.Base64.Lazy as Base64L
 import Foundation
 --import Model
 
+
+import Web.LTI
+import Control.Monad.Trans.Except (runExceptT)
+import qualified Data.Map as Map
+
+t_oauth_consumer_key :: BS.ByteString
+t_oauth_consumer_key = "test_lti_key"
+
+t_oauth_secret :: BS.ByteString
+t_oauth_secret = "test_lti_secret"
+
+t_lti :: LTIProvider
+t_lti = newLTIProvider t_oauth_consumer_key t_oauth_secret
 
 data Story = Story
     { storyAuthor  :: Maybe Text
@@ -56,44 +70,17 @@ instance Show Story where
         ++ "Comment: " ++ show (storyComment s)
 
 getImageUploadR :: Handler Html
-getImageUploadR = do
-    ((res, widget), formEncType) <- runFormPost uploadForm
-    defaultLayout $ do
-      setTitle "Share your story"
-      case res of
-        FormFailure msgs -> showFormError msgs
-        _ -> return ()
-      showFormWidget widget formEncType
-  where
-    showFormError :: [Text] -> Widget
-    showFormError msgs = do
-      errorWrapper <- newIdent
-      toWidget [cassius|
-        ##{errorWrapper} div
-            background-color: #fcc;
-            padding: 3px 5%;
-            margin: 3px;
-            font-variant: all-small-caps;
-      |]
-      [whamlet|
-        <div ##{errorWrapper}>
-          $forall msg <- msgs
-            <div>#{msg}
-      |]
-    showFormWidget :: Widget -> Enctype -> Widget
-    showFormWidget widget formEncType = [whamlet|
-        <form method=post action=@{ImageUploadR} enctype=#{formEncType}>
-          ^{widget}
-      |]
+getImageUploadR = postImageUploadR
 
 postImageUploadR :: Handler Html
-postImageUploadR = do
+postImageUploadR = setupSession $ \userId -> do
     ((res, widget), formEncType) <- runFormPost uploadForm
     defaultLayout $ do
       setTitle "Share your story"
       case res of
         FormFailure msgs -> showFormError msgs
         _ -> return ()
+      [whamlet|<p>#{userId}|]
       showFormWidget widget formEncType
   where
     showFormError :: [Text] -> Widget
@@ -117,14 +104,38 @@ postImageUploadR = do
           ^{widget}
       |]
 
+setupSession :: (Text -> Handler Html) -> Handler Html
+setupSession h = do
+    muid <- lookupSession "lis_result_sourcedid"
+    case muid of
+      Nothing -> do
+        wreq <- waiRequest
+        eltiRequest <- runExceptT $ processWaiRequest t_lti wreq
+        case eltiRequest of
+          Left err -> defaultLayout $ do
+            setTitle "Error"
+            [whamlet|<p>#{show err}|]
+          Right pams -> do
+            let murl = Text.decodeUtf8 <$> Map.lookup "lis_outcome_service_url" pams
+                msid = Text.decodeUtf8 <$> Map.lookup "lis_result_sourcedid" pams
+            Prelude.mapM_ (setSession "lis_outcome_service_url") murl
+            Prelude.mapM_ (setSession "lis_result_sourcedid") msid
+            case msid of
+                Nothing -> defaultLayout $ do
+                    setTitle "Error"
+                    [whamlet|<p>Failed to lookup lis_result_sourcedid|]
+                Just uid -> h uid
+      Just uid -> h uid
 
 uploadForm :: Html -> MForm Handler (FormResult Story, Widget)
 uploadForm extra = do
+    -- set up ids of main div elements
     storyDataDiv  <- newIdent
     imgPreviewDiv <- newIdent
     topDiv    <- newIdent
     bottomDiv <- newIdent
 
+    -- set up all input
     (authorRes, authorView ) <- mopt textField     opts Nothing
     (mimageRes, imageView  ) <- mopt fileField     reqs
        { fsAttrs = ("accept","image/*") : fsAttrs reqs} Nothing
@@ -133,35 +144,22 @@ uploadForm extra = do
     (commentRes,commentView) <- mreq textareaField reqs Nothing
     (agreeRes,  agreeView  ) <- mreq checkBoxField reqs Nothing
 
+    -- set up hidden fields for keeping image
     (imgFName, imgFType, imgBase64) <- case mimageRes of
         FormSuccess (Just fi) -> do
-            liftIO $ print $ fileName fi
             fb <- runResourceT $ fileSource fi $$ sinkLbs
             return ( Just $ fileName fi
                    , Just $ fileContentType fi
                    , Just . LText.decodeUtf8 $ Base64L.encode fb
                    )
         _ -> return (Nothing, Nothing, Nothing)
-
-
     (inameRes, inameView) <- addHiddenValueHolder imgFName
     (itypeRes, itypeView) <- addHiddenValueHolder imgFType
     (idataRes, idataView) <- addHiddenValueHolder imgBase64
 
-    let mLastImage = formToMaybe $ mTriple
-            <$> inameRes
-            <*> itypeRes
-            <*> idataRes
-        mTriple (Just a) (Just b) (Just c) = Just (FileInfo
-                { fileName = a
-                , fileContentType = b
-                , fileSourceRaw = sourceLbs . Base64L.decodeLenient
-                                            $ LText.encodeUtf8 c
-                , fileMove = \_ -> return ()
-                }, c)
-        mTriple _ _ _ = Nothing
-
-        imageRes = case (mimageRes, fst <$> mLastImage) of
+    let -- take either current image or previous (if any)
+        imageRes = case ( mimageRes
+                        , makeFileInfo inameRes itypeRes idataRes) of
             (FormSuccess (Just x), _)       -> FormSuccess x
             (FormSuccess Nothing , Just x ) -> FormSuccess x
             (FormSuccess Nothing , Nothing) -> FormFailure ["Value is required"]
@@ -171,14 +169,14 @@ uploadForm extra = do
             (FormFailure errs    , Nothing) -> FormFailure errs
 
         setErrMsg s v = case v of
-                FormFailure (m:ms) ->
-                    FormFailure ((s `Text.append` ": " `Text.append` m):ms)
-                x -> x
+            FormFailure (m:ms) ->
+                FormFailure ((s `Text.append` ": " `Text.append` m):ms)
+            x -> x
 
         mustAgree s = case agreeRes of
-                FormSuccess False ->
-                    FormFailure ["You must agree the terms of use (check the checkbox)"] *> s
-                x -> x *> s
+            FormSuccess False ->
+                FormFailure ["You must agree the terms of use (check the checkbox)"] *> s
+            x -> x *> s
 
         storyRes = mustAgree $ Story
             <$> setErrMsg "Error in the field \"author\"" authorRes
@@ -186,18 +184,16 @@ uploadForm extra = do
             <*> setErrMsg "Error in the field \"country\"" countryRes
             <*> setErrMsg "Error in the field \"city\"" cityRes
             <*> setErrMsg "Error in the field \"commentary\"" commentRes
-    imageBytes <- case imageRes of
-      FormSuccess fi -> do
-        fb <- runResourceT $ fileSource fi $$ sinkLbs
-        return . Just $ "data:"
-            `LText.append` (LText.fromStrict $ fileContentType fi)
-            `LText.append` ";base64,"
-            `LText.append` (LText.decodeLatin1 $ Base64L.encode fb)
-      _ -> return Nothing
+
+        -- insert the image encoded data into the page later
+        imageBytes = case (itypeRes, idataRes) of
+          (Just typ, Just dat) -> Just $ "data:"
+                `Text.append` typ
+                `Text.append` ";base64,"
+                `Text.append` (LText.toStrict dat)
+          _ -> Nothing
     return (storyRes, $(widgetFileNoReload def "imageUpload"))
   where
-    formToMaybe (FormSuccess (Just x)) = Just x
-    formToMaybe _ = Nothing
     reqs = FieldSettings
         { fsLabel   = ""
         , fsTooltip = Nothing
@@ -212,23 +208,39 @@ uploadForm extra = do
         , fsName    = Nothing
         , fsAttrs   = []
         }
+    makeFileInfo (Just iname)
+                 (Just itype)
+                 (Just idata) = Just $ FileInfo
+        { fileName = iname
+        , fileContentType = itype
+        , fileSourceRaw = sourceLbs . Base64L.decodeLenient
+                                    $ LText.encodeUtf8 idata
+        , fileMove = \_ -> return ()
+        }
+    makeFileInfo _ _ _ = Nothing
+
+
+-- | Returns default value if it is given,
+--   otherwise returns input value
 addHiddenValueHolder :: (PathPiece p, Blaze.ToMarkup p)
                      => Maybe p
-                     -> MForm Handler (FormResult (Maybe p), Widget)
-addHiddenValueHolder Nothing = return (FormMissing, [whamlet||])
+                     -> MForm Handler (Maybe p, Widget)
 addHiddenValueHolder mx = do
-  xId <- newIdent
   xName <- newIdent
-  (xRes, _) <- mopt hiddenField FieldSettings
+  (xRes', xView) <- mopt hiddenField FieldSettings
     { fsLabel   = ""
     , fsTooltip = Nothing
-    , fsId      = Just xId
+    , fsId      = Nothing
     , fsName    = Just xName
     , fsAttrs   = []
     }  Nothing
-  return $ case mx of
-    Nothing -> ( xRes, [whamlet||])
-    Just x  -> ( xRes
-               , [whamlet|<input type="hidden" ##{xId} name="#{xName}" value="#{x}">|])
-
+  let xRes = case mx of
+        Nothing -> case xRes' of
+           FormSuccess (Just x) -> Just x
+           _                    -> Nothing
+        Just x  -> Just x
+  return . (,) xRes $ case xRes of
+    Just x ->
+        [whamlet|<input type="hidden" ##{fvId xView} name="#{xName}" value="#{x}">|]
+    _ -> return ()
 
