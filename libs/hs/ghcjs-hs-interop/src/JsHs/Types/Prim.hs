01d9486c04e9573e06513f30c08e67ffd15ee231
achirkin <chirkin@arch.ethz.ch>
2016-05-20 10:26:55 +0200

    In the middle of refactoring to ghcjs-hs-interop

---
+++ libs/hs/ghcjs-hs-interop/src/JsHs/Types/Prim.hs
@@ -0,0 +1,18 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  JsHs.Types.Prim
+-- Copyright   :  Artem Chirkin
+-- License     :  MIT
+--
+-- Maintainer  :  Artem Chirkin
+-- Stability   :  experimental
+--
+--
+-----------------------------------------------------------------------------
+
+module JsHs.Types.Prim
+    ( JSVal (..)
+    ) where
+
+
+import GHCJS.Prim (JSVal (..))
