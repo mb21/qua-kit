e7429e4a40fb013b52b274ef79b5d204fd86eaaf
Chirkin  Artem <achirkin@ethz.ch>
2017-02-17 14:09:48 +0100

    Updated criteria list

--- apps/hs/qua-server/src/Application/SetupProblemData.hs
+++ apps/hs/qua-server/src/Application/SetupProblemData.hs
@@ -36,10 +36,10 @@ importProblemRun0 pool = do
     sctaskfile <- BS.readFile $ df </> "mooctask.geojson"
     sctaskpreview <- BS.readFile $ df </> "mooctask.png"
 
-    cCentrality    <- loadCriterion df "Centrality"
-    cConnectivity  <- loadCriterion df "Connectivity"
-    cAccessibility <- loadCriterion df "Accessibility"
-    cVisibility    <- loadCriterion df "Visibility"
+    cCentrality    <- loadCriterion df "Centrality" "Centrality"
+    cConnectivity  <- loadCriterion df "Connectivity" "Connectivity"
+    cAccessibility <- loadCriterion df "Accessibility" "Accessibility"
+    cVisibility    <- loadCriterion df "Visibility" "Visibility"
 
     flip runSqlPool pool $ do
       -- Id of the firt problem (Sep-Nov 2016)
@@ -65,24 +65,24 @@ importProblemRun1 pool = do
     sctaskfile <- BS.readFile $ df </> "mooctask.geojson"
     sctaskpreview <- BS.readFile $ df </> "mooctask.png"
 
-    cCentrality    <- loadCriterion df "Centrality"
-    cConnectivity  <- loadCriterion df "Connectivity"
-    cAccessibility <- loadCriterion df "Accessibility"
-    cVisibility    <- loadCriterion df "Visibility"
+    cDistribution  <- loadCriterion df "Density / Distribution" "Distribution"
+    cAccessibility <- loadCriterion df "Accessibility / Connectivity" "AccConn"
+    cVisibility    <- loadCriterion df "Visibility" "Visibility"
+    cCentrality    <- loadCriterion df "Centrality" "Centrality"
 
     flip runSqlPool pool $ do
       -- Id of the firt problem (Sep-Nov 2016)
       let pId = toSqlKey 1
       repsert pId (ScenarioProblem sctaskpreview sctaskfile "Empower Shack scenario 2" 40000)
-      cIdCentrality    <- upsertCriterion cCentrality
-      cIdConnectivity  <- upsertCriterion cConnectivity
+      cIdDistribution  <- upsertCriterion cDistribution
       cIdAccessibility <- upsertCriterion cAccessibility
       cIdVisibility    <- upsertCriterion cVisibility
+      cIdCentrality    <- upsertCriterion cCentrality
 
       -- register criteria in the problem
       deleteWhere [ProblemCriterionProblemId ==. pId]
       insert_ (ProblemCriterion pId cIdCentrality)
-      insert_ (ProblemCriterion pId cIdConnectivity)
+      insert_ (ProblemCriterion pId cIdDistribution)
       insert_ (ProblemCriterion pId cIdAccessibility)
       insert_ (ProblemCriterion pId cIdVisibility)
 
@@ -91,12 +91,12 @@ importProblemRun1 pool = do
 
 
 -- | Load criterion content from a file
-loadCriterion :: FilePath -> String -> IO Criterion
-loadCriterion fp name = do
-    cHtml <- Text.readFile $ fp </> name <.> "html"
-    cImg  <- BS.readFile $ fp </> name <.> "png"
-    cIcon <- fmap decodeUtf8 . BS.readFile $ fp </> name <.> "svg"
-    return $ Criterion (pack name) cHtml cImg cIcon
+loadCriterion :: FilePath -> Text -> String -> IO Criterion
+loadCriterion fp name fname = do
+    cHtml <- Text.readFile $ fp </> fname <.> "html"
+    cImg  <- BS.readFile $ fp </> fname <.> "png"
+    cIcon <- fmap decodeUtf8 . BS.readFile $ fp </> fname <.> "svg"
+    return $ Criterion name cHtml cImg cIcon
 
 -- | Update or create criterion defenition
 upsertCriterion :: MonadIO m
--- apps/hs/qua-server/src/Handler/Mooc/Criteria.hs
+++ apps/hs/qua-server/src/Handler/Mooc/Criteria.hs
@@ -16,33 +16,101 @@ module Handler.Mooc.Criteria
   ) where
 
 import Import
-import Text.Blaze
+import Text.Blaze as Blaze
+import Database.Persist.Sql
+import Data.Text.Read (decimal)
 
 getCriteriaListR :: Handler Html
 getCriteriaListR = do
-    criteria <- runDB $ selectList [] []
+    mtscp_id <- lookupSession "custom_exercise_id"
+    max_scp_id <- runDB lastScenarioProblemId
+    let scp_id = case decimal <$> mtscp_id of
+          Just (Right (i, _)) -> toSqlKey i
+          _ -> max_scp_id
+
+    criteria <- runDB $ currentCriteria scp_id
+    oldCriteria <- runDB $ otherCriteria scp_id
+
     fullLayout Nothing "Qua-kit design criteria" $ do
       setTitle "Qua-kit design criteria"
       toWidgetBody $
         [hamlet|
           <div class="ui-card-wrap">
             <div class="row">
-              $forall (Entity cId criterion) <- criteria
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                  <div class="card">
-                    <aside class="card-side card-side-img pull-left card-side-moocimg">
-                      <img src="@{CriteriaImgR cId}">
-                    <div class="card-main">
-                      <div.card-header>
-                        <div.card-inner>
-                          <h5.h5.margin-bottom-no.margin-top-no.text-brand-accent>
-                            #{criterionName criterion}
-                      <div class="card-inner" style="margin: 10px 12px;">
-                        #{preEscapedText $ criterionDescription criterion}
+              $forall crit <- criteria
+                ^{criterionCard crit}
+
+          <div class="ui-card-wrap">
+            <div class="row">
+              <div class="col-lg-7 col-md-8 col-sm-9">
+                <div class="card card-red">
+                  <div class="card-main">
+                    <div class="card-inner">
+                      <p>
+                        Next, you can find a list of criteria we used
+                        for the previous runs of this online course.
+                        <br>
+                        You do not need them, but we keep the whole list here for the
+                        sake of completeness.
+
+          <div class="ui-card-wrap">
+            <div class="row">
+              $forall crit <- oldCriteria
+                ^{criterionCard crit}
         |]
+  where
+    criterionCard (Entity cId criterion) =
+      [hamlet|
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <div class="card">
+            <aside class="card-side card-side-img pull-left card-side-moocimg">
+              <img src="@{CriteriaImgR cId}">
+            <div class="card-main">
+              <div.card-header>
+                <div.card-inner>
+                  <h5.h5.margin-top-no.text-brand-accent style="display: inline-block; vertical-align: middle; margin-bottom: -4px;">
+                    #{Blaze.preEscapedToMarkup $ criterionIcon criterion}
+                  <h5.h5.margin-bottom-no.margin-top-no.text-brand-accent style="display: inline; vertical-align: middle">
+                    #{criterionName criterion}
+              <div class="card-inner" style="margin: 10px 12px;">
+                #{preEscapedText $ criterionDescription criterion}
+      |]
 
 getCriteriaImgR :: CriterionId -> Handler TypedContent
 getCriteriaImgR ident = do
     criterion <- runDB $ get404 ident
     addHeader "Content-Disposition" "inline"
     sendResponse (("image/png" :: ByteString), toContent $ criterionImage criterion)
+
+
+lastScenarioProblemId :: ReaderT SqlBackend Handler ScenarioProblemId
+lastScenarioProblemId = getVal <$> rawSql query []
+  where
+    getVal (Single c:_)  = c
+    getVal [] = toSqlKey 0
+    query = unlines
+          ["SELECT max(id) FROM scenario_problem;"
+          ]
+
+currentCriteria :: ScenarioProblemId -> ReaderT SqlBackend Handler [Entity Criterion]
+currentCriteria scp_id = rawSql query [toPersistValue scp_id]
+  where
+    query = unlines
+        ["SELECT ?? FROM criterion,problem_criterion"
+        ,"         WHERE problem_criterion.problem_id = ?"
+        ,"           AND problem_criterion.criterion_id = criterion.id;"
+        ]
+
+otherCriteria :: ScenarioProblemId -> ReaderT SqlBackend Handler [Entity Criterion]
+otherCriteria scp_id = rawSql query [toPersistValue scp_id]
+  where
+    query = unlines
+        ["SELECT ??"
+        ,"  FROM criterion"
+        ," WHERE criterion.id NOT IN"
+        ," ("
+        ,"  SELECT criterion.id FROM criterion,problem_criterion"
+        ,"                     WHERE problem_criterion.problem_id = ?"
+        ,"                       AND problem_criterion.criterion_id = criterion.id"
+        ," );"
+        ]
