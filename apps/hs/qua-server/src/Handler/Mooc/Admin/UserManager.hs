462423d4271f476e00b31e45755384ecee5f4cd1
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-07-06 10:23:10 +0200

    A route for the user manager

---
+++ apps/hs/qua-server/src/Handler/Mooc/Admin/UserManager.hs
@@ -0,0 +1,20 @@
+{-# RecordWildCards #-}
+module Handler.Mooc.Admin.UserManager
+    ( getAdminUserManagerR
+    ) where
+
+import Import hiding ((==.), on)
+
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.Conduit.Binary as CB
+import qualified Data.Function as Function (on)
+import qualified Data.List as List (groupBy, head)
+import qualified Data.Text as T
+
+import Database.Esqueleto
+import qualified Database.Persist as P
+
+import Yesod.Form.Bootstrap3
+
+getAdminUserManagerR :: Handler Html
+getAdminUserManagerR = pure mempty
