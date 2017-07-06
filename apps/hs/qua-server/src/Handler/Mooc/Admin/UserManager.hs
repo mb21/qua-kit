6ec43d5e618ca6bff3aa4442357ca3252b43cfd8
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-07-06 11:35:46 +0200

    A button to change roles

--- apps/hs/qua-server/src/Handler/Mooc/Admin/UserManager.hs
+++ apps/hs/qua-server/src/Handler/Mooc/Admin/UserManager.hs
@@ -1,6 +1,7 @@
 {-# RecordWildCards #-}
 module Handler.Mooc.Admin.UserManager
     ( getAdminUserManagerR
+    , postSetUserRoleR
     ) where
 
 import Import hiding ((==.), on)
@@ -10,6 +11,7 @@ import qualified Data.Conduit.Binary as CB
 import qualified Data.Function as Function (on)
 import qualified Data.List as List (groupBy, head)
 import qualified Data.Text as T
+import Text.Read
 
 import Database.Persist
 import Database.Persist.Sql
@@ -19,6 +21,19 @@ import Yesod.Form.Bootstrap3
 getAdminUserManagerR :: Handler Html
 getAdminUserManagerR = do
     users <- runDB $ selectList [] [Asc UserId]
+    let roles = [minBound .. maxBound] :: [UserRole]
     fullLayout Nothing "Welcome to the user manager" $ do
         setTitle "qua-kit - user manager"
         $(widgetFile "mooc/admin/user-manager")
+
+roleFormInput :: FormInput Handler (Maybe UserRole)
+roleFormInput = (readMaybe . T.unpack) <$> ireq textField "role"
+
+postSetUserRoleR :: UserId -> Handler Html
+postSetUserRoleR userId = do
+    mrole <- runInputPost roleFormInput
+    case mrole of
+        Nothing -> sendResponseStatus status400 ("invalid role" :: Text)
+        Just role -> do
+            runDB $ update userId [UserRole =. role]
+            redirect AdminUserManagerR
