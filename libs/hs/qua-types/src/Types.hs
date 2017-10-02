ae7b72116edfea79f81941787ceef59b9df689ef
mb21 <mb21@users.noreply.github.com>
2017-10-02 13:04:23 +0200

    Types: Text -> Text/JSSTring

--- libs/hs/qua-types/src/Types.hs
+++ libs/hs/qua-types/src/Types.hs
@@ -5,19 +5,29 @@
 module Types
     ( ReviewPost (..)
     , ThumbState (..)
+    , ReviewSettings (..)
     , Settings   (..)
     ) where
 
 import Data.Semigroup
-import Data.Text
 import GHC.Generics
 
 #ifdef ghcjs_HOST_OS
+import Data.JSString
 import JavaScript.JSON.Types.Internal ()
 import JavaScript.JSON.Types.Instances
 import JavaScript.JSON.Types.Generic ()
+
+type QuaText = JSString
+emptyQuaText = Data.JSString.null
+
 #else
+
 import Data.Aeson
+import Data.Text
+
+type QuaText = Text
+emptyQuaText = Data.Text.null
 #endif
 
 -- | Left-biased choice on foldables
@@ -26,8 +36,8 @@ x `orElse` y = if Prelude.null x
                  then y
                  else x
 
-orElseText :: Text -> Text -> Text
-x `orElseText` y = if Data.Text.null x
+orElseText :: QuaText -> QuaText -> QuaText
+x `orElseText` y = if emptyQuaText x
                      then y
                      else x
 
@@ -37,18 +47,19 @@ instance FromJSON ThumbState
 
 data ReviewPost = ReviewPost {
     thumb             :: !ThumbState
-  , reviewPostComment :: !Text
+  , reviewPostComment :: !QuaText
   } deriving Generic
 instance ToJSON   ReviewPost
 instance FromJSON ReviewPost
 
 data Settings = Settings {
-    loggingUrl              :: Maybe Text -- ^ WebSocket URL to send user analytics to
-  , luciUrl                 :: Maybe Text -- ^ WebSocket URL to connect to Luci
-  , getSubmissionGeoJsonUrl :: Maybe Text -- ^ URL to GET geoJSON for current submission
-  , postSubmissionUrl       :: Maybe Text -- ^ URL for students to POST their new submission to
-  , viewMode                :: Text       -- TODO: use proper type from qua-server
-  , viewUrl                 :: Text       -- ^ URL of current qua-viewer page
+    loggingUrl              :: Maybe QuaText -- ^ WebSocket URL to send user analytics to
+  , luciUrl                 :: Maybe QuaText -- ^ WebSocket URL to connect to Luci
+  , getSubmissionGeoJsonUrl :: Maybe QuaText -- ^ URL to GET geoJSON for current submission
+  , postSubmissionUrl       :: Maybe QuaText -- ^ URL for students to POST their new submission to
+  , reviewSettingsUrl       :: Maybe QuaText
+  , viewMode                :: QuaText       -- TODO: use proper type
+  , viewUrl                 :: QuaText       -- ^ URL of current qua-viewer page
   } deriving Generic
 instance ToJSON    Settings
 instance FromJSON  Settings
@@ -58,6 +69,7 @@ instance Semigroup Settings where
                , luciUrl                 = orElse (luciUrl s1) (luciUrl s2)
                , getSubmissionGeoJsonUrl = orElse (getSubmissionGeoJsonUrl s1) (getSubmissionGeoJsonUrl s2)
                , postSubmissionUrl       = orElse (postSubmissionUrl s1) (postSubmissionUrl s2)
+               , reviewSettingsUrl       = orElse (reviewSettingsUrl s1) (reviewSettingsUrl s2)
                , viewMode                = orElseText (viewMode s1) (viewMode s2)
                , viewUrl                 = orElseText (viewUrl s1) (viewUrl s2)
                }
@@ -68,6 +80,13 @@ instance Monoid Settings where
            , luciUrl                 = Nothing
            , getSubmissionGeoJsonUrl = Nothing
            , postSubmissionUrl       = Nothing
+           , reviewSettingsUrl       = Nothing
            , viewMode                = ""
            , viewUrl                 = ""
            }
+
+data ReviewSettings = ReviewSettings {
+    postReviewUrl :: QuaText
+  } deriving Generic
+instance ToJSON    ReviewSettings
+instance FromJSON  ReviewSettings
