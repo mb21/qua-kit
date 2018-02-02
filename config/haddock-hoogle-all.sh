a06a28cfd753343f43d4733536d2d0ce0a19a554
achirkin <chirkin@arch.ethz.ch>
2018-02-02 11:01:53 +0100

    updated haddock script

--- config/haddock-hoogle-all.sh
+++ config/haddock-hoogle-all.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-stack haddock --stack-yaml=apps/hs/qua-view/stack.yaml
-stack haddock --stack-yaml=apps/hs/qua-server/stack.yaml
-stack haddock --stack-yaml=apps/hs/helen/stack.yaml
-stack haddock --stack-yaml=services/siren/stack.yaml
-stack haddock --stack-yaml=libs/hs/luci-connect/stack.yaml
+stack build --haddock --haddock-deps --haddock-hyperlink-source --stack-yaml=apps/hs/qua-view/stack.yaml --haddock-arguments --hoogle
+stack build --haddock --haddock-deps --haddock-hyperlink-source --stack-yaml=apps/hs/qua-server/stack.yaml
+stack build --haddock --haddock-deps --haddock-hyperlink-source --stack-yaml=apps/hs/helen/stack.yaml
+stack build --haddock --haddock-deps --haddock-hyperlink-source --stack-yaml=services/siren/stack.yaml
+stack build --haddock --haddock-deps --haddock-hyperlink-source --stack-yaml=libs/hs/luci-connect/stack.yaml
+stack build --haddock --haddock-deps --haddock-hyperlink-source --stack-yaml=libs/hs/qua-types/stack.yaml
 stack exec hoogle -- generate \
                   --local=$(stack path --stack-yaml=apps/hs/qua-server/stack.yaml --snapshot-doc-root) \
                   --local=$(stack path --stack-yaml=apps/hs/qua-view/stack.yaml --snapshot-doc-root) \
@@ -11,4 +12,5 @@ stack exec hoogle -- generate \
                   --local=$(stack path --stack-yaml=apps/hs/qua-server/stack.yaml --local-doc-root) \
                   --local=$(stack path --stack-yaml=apps/hs/helen/stack.yaml --local-doc-root) \
                   --local=$(stack path --stack-yaml=services/siren/stack.yaml --local-doc-root) \
-                  --local=$(stack path --stack-yaml=libs/hs/luci-connect/stack.yaml --local-doc-root)
+                  --local=$(stack path --stack-yaml=libs/hs/luci-connect/stack.yaml --local-doc-root) \
+                  --local=$(stack path --stack-yaml=libs/hs/qua-types/stack.yaml --local-doc-root)
